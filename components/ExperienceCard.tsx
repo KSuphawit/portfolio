67b716cdc95102e096e5a377fa07bd49f31879b5
Ksuphawit <ksuphawit@Ksuphawits-MacBook-Pro.local>
2023-11-01 15:17:03 +0700

    fix work experince section for mobile size

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 function ExperienceCard({ experience }: Props) {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[320px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <motion.img
         initial={{
           y: -100,
@@ -19,30 +19,34 @@ function ExperienceCard({ experience }: Props) {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         src={urlFor(experience.companyImage).url()}
-        className="w-32 h-32 rounded-full md:rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+        className="w-24 h-24 md:w-32 md:h-32 rounded-full md:rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
       />
 
       <div className="px-0 md:px-10 ">
-        <h4 className="text-4xl font-light">{experience.jobTitle}</h4>
-        <p className="font-bold text-2xl mt-1">{experience.company}</p>
+        <h4 className="text-lg md:text-4xl font-light">
+          {experience.jobTitle}
+        </h4>
+        <p className="font-bold text-md md:text-2xl mt-1">
+          {experience.company}
+        </p>
         <div className="flex space-x-2 my-2">
           {experience.technologies.map((technology) => (
             <img
               key={technology._id}
-              className="h-10 w-10 rounded-full"
+              className="h-6 w-6 md:h-10 md:w-10 rounded-full"
               src={urlFor(technology.image).url()}
             />
           ))}
         </div>
 
-        <p className="uppercase py-5 text-gray-300">
+        <p className="uppercase text-xs md:text-lg py-5 text-gray-300">
           {new Date(experience.dateStarted).toDateString()} -{" "}
           {experience.isCurrentlyWorkingHere
             ? "Present"
             : new Date(experience.dateEnded).toDateString()}
         </p>
 
-        <ul className="list-disc space-y-4 ml-5 text-lg">
+        <ul className="list-disc space-y-4 ml-5 text-xs md:text-base">
           {experience.points.map((point, i) => (
             <li key={i}>{point}</li>
           ))}
