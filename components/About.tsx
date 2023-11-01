2f5d98075fece353ab5e88d18d95d95ca8dcff20
Ksuphawit <ksuphawit@Ksuphawits-MacBook-Pro.local>
2023-11-01 15:05:09 +0700

    fix about mobile site

--- components/About.tsx
+++ components/About.tsx
@@ -16,7 +16,7 @@ function About({ pageInfo }: Props) {
       transition={{ duration: 1.5 }}
       className="flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 h-screen justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[10px] md:tracking-[20px] text-gray-500 text-xl md:text-2xl">
         About
       </h3>
 
@@ -28,16 +28,16 @@ function About({ pageInfo }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src={urlFor(pageInfo?.profilePic).url()}
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] object-cover"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-44 h-44 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] object-cover"
       ></motion.img>
 
       <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl font-semibold">
+        <h4 className="text-2xl md:text-4xl font-semibold">
           Here is a{" "}
           <span className="decoration-[#F7AB0A]/50 underline">little</span>{" "}
           background
         </h4>
-        <p className="text-sm md:text-lg">{pageInfo?.backgroundInformation}</p>
+        <p className="text-md md:text-lg">{pageInfo?.backgroundInformation}</p>
       </div>
     </motion.div>
   );
