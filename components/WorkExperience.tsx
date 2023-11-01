0cea55cc30427e2c85031a1fdeb879eb14ac762e
Ksuphawit <ksuphawit@Ksuphawits-MacBook-Pro.local>
2023-11-01 22:01:50 +0700

    order work exp

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import ExperienceCard from "./ExperienceCard";
 import { Experience } from "../typings";
+import Link from "next/link";
 
 type Props = {
   experiences: Experience[];
